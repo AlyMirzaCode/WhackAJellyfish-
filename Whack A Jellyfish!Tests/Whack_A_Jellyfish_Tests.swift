a05fc6575a28e2a17c14c4e9de18a539018d891f
Aly Mirza <a5mirza@uwaterloo.ca>
2018-03-22 20:10:33 -0400

    Whack A Jellyfish

---
+++ Whack A Jellyfish!Tests/Whack_A_Jellyfish_Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Whack_A_Jellyfish_Tests.swift
+//  Whack A Jellyfish!Tests
+//
+//  Created by Aly Mirza on 2017-10-31.
+//  Copyright © 2017 Aly Mirza. All rights reserved.
+//
+
+import XCTest
+@testable import Whack_A_Jellyfish_
+
+class Whack_A_Jellyfish_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
